51510551b9299001eeca83f83c71f185eba5d976
area268457913 <area268457913@hotmail.com>
2021-10-25 00:20:47 +0800

    feat: add relations between User and Todo models

--- app.js
+++ app.js
@@ -34,6 +34,12 @@ app.use(bodyParser.urlencoded({ extended: true }))
 // 設定每一筆請求都會透過 methodOverride 進行前置處理
 app.use(methodOverride('_method'))
 usePassport(app)
+app.use((req, res, next) => {
+  // 你可以在這裡 console.log(req.user) 等資訊來觀察
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  next()
+})
 app.use(routes)
 
 // app.get('/', (req, res) => {
