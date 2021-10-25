f6dd6e279779d3616e5913121d365541445d5805
area268457913 <area268457913@hotmail.com>
2021-10-25 14:16:17 +0800

    add bcrypt.js

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 // 載入 method-override
 const methodOverride = require('method-override')
+const flash = require('connect-flash')
 const routes = require('./routes')
 
 require('./config/mongoose')
@@ -34,10 +35,13 @@ app.use(bodyParser.urlencoded({ extended: true }))
 // 設定每一筆請求都會透過 methodOverride 進行前置處理
 app.use(methodOverride('_method'))
 usePassport(app)
+app.use(flash())
 app.use((req, res, next) => {
   // 你可以在這裡 console.log(req.user) 等資訊來觀察
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')  // 設定 success_msg 訊息
+  res.locals.warning_msg = req.flash('warning_msg')  // 設定 warning_msg 訊息
   next()
 })
 app.use(routes)
